394426bc6198d8975d244ff259e65bd8dab7301c
Yifan Gu <gyf304@gmail.com>
2021-07-11 01:40:57 -0400

    fix release

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name="termimg",
-    version="0.1.0b0",
+    name="pytermimg",
+    version="0.1.0b1",
     python_requires='>3.6',
-    packages=["termimg"],
+    packages=["termimg", "termimg.printers"],
     license="MIT",
     long_description=open("README.md").read(),
     author="Yifan Gu",
-    author_email="dev@yifangu.com"
+    author_email="dev@yifangu.com",
+    install_requires=["pillow>=8.3.1"]
 )
